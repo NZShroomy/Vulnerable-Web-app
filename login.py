302163d83348395a65246978e280edf3ba9fdd9b
Jay <jack_crichton@yahoo.co.nz>
2025-11-25 19:27:12 +1300

    Setup of Login Logic
    including navigation, forgot password etc

--- login.py
+++ login.py
@@ -1,1 +1,27 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 import sqlite3
+from CreateDB import DB_NAME
+
+login_bp = Blueprint('login_bp',__name__)
+
+@login_bp.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        conn = sqlite3.connect(DB_NAME)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            flash("Login successful", "success")
+            return redirect(url_for('homepage')) #no login state kept
+        else:
+            flash("Invald user credentials", "error")
+            return redirect(url_for('login_bp.login'))
+    
+    return render_template("Login.html")
